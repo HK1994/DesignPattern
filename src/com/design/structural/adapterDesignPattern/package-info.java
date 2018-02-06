68fcaf593b4d906d4b3deed7ccadc3df518d690e
Hemant <hemantkumarvs94@gmail.com>
2018-02-06 14:44:26 +0530

    Adapter detail

--- src/com/design/structural/adapterDesignPattern/package-info.java
+++ src/com/design/structural/adapterDesignPattern/package-info.java
@@ -2,3 +2,27 @@
  * Created by hemantkumar on 2/6/2018.
  */
 package com.design.structural.adapterDesignPattern;
+
+/*
+*  it's a great pattern for connecting new code to legacy code without having to change the working contract that was produced from the legacy code originally.
+*  Real example is plug adapter but it doesn't cover all it's features
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
