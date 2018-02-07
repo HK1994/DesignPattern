9d08bfa9dba4612e10c63c14d4a638ce9c3b33ef
Hemant <hemantkumarvs94@gmail.com>
2018-02-07 12:37:47 +0530

    Structural package info

--- src/com/design/structural/package-info.java
+++ src/com/design/structural/package-info.java
@@ -1,4 +1,7 @@
 /**
  * Created by hemantkumar on 2/6/2018.
+ * Structural Design Pattern
  */
 package com.design.structural;
+//Structural patterns are focused on how you use or utilize objects.
+// It could be for something like performance or refactoring, or memory utilization just to name a few concepts
